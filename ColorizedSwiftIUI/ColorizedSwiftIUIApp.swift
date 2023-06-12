58c0d0c12961f7c7122e58cfde7a7134754f85a4
Panferov.creator <panferov.creator@gmail.com>
2023-06-12 19:22:14 +0500

    Initial Commit

---
+++ ColorizedSwiftIUI/ColorizedSwiftIUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftIUIApp.swift
+//  ColorizedSwiftIUI
+//
+//  Created by Dmitriy Panferov on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftIUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
