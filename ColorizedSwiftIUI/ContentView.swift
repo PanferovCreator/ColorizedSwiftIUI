36ff04728a79daec28267579cfc16d078325c25f
Panferov.creator <panferov.creator@gmail.com>
2023-06-13 16:01:54 +0500

    Work with logic and UI

--- ColorizedSwiftIUI/ContentView.swift
+++ ColorizedSwiftIUI/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ColorView(color: Color(
+                    red: redSliderValue / 255,
+                    green: greenSliderValue / 255,
+                    blue: blueSliderValue / 255
+                )
+            )
+                      
+            VStack {
+                ColorSliderView(value: $redSliderValue, sliderColor: .red)
+                ColorSliderView(value: $greenSliderValue, sliderColor: .green)
+                ColorSliderView(value: $blueSliderValue, sliderColor: .blue)
+            }
+            
+            Spacer()
         }
         .padding()
+        .background(Color.cyan)
     }
 }
 
@@ -24,3 +40,19 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+    var sliderColor: Color
+
+    var body: some View {
+        HStack(spacing: 10) {
+            Text("\(String(lround(value)))")
+                .foregroundColor(.white)
+                .frame(width: 40)
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(sliderColor)
+        }
+    }
+}
+
